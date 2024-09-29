483a3051f5ea9edd790a87f2908e3fe9403c0244
baselbalkes <baselbalkes77@gmail.com>
2024-09-29 20:31:13 +0300

    fix build errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,11 @@
 import HeaderPage from "@/components/HeaderPage/HeaderPage";
 import styles from "./page.module.css";
-import New from "@/components/New/New";
-import { Suspense } from "react";
-import { newsFetch } from "@/dataFetching/newsFetch";
 
-export default async function Home() {
-  const result = await newsFetch();
+export default function Home() {
   return (
     <div>
       <HeaderPage props={{title: "Latest News", image:"/images/home/news.ico"}}/>
-      <div className={styles.newsContainer}>
-        {result.map((newInfo)=>{
-          return(
-            <Suspense key={newInfo.id} fallback={<p>loading ...</p>}>
-              <New props={{title: newInfo.title, content: newInfo.content, image: newInfo.image, created_at: newInfo.created_at}} />
-            </Suspense>
-          )
-        })}
-      </div>
+      <div className={styles.newsContainer}>hhh</div>
     </div>
   );
 }
