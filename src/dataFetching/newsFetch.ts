7eb09a32a4e6266c7686a61ea8ac956783aed8f3
baselbalkes <baselbalkes77@gmail.com>
2024-09-29 21:27:40 +0300

    fix url error

--- src/dataFetching/newsFetch.ts
+++ src/dataFetching/newsFetch.ts
@@ -1,7 +1,7 @@
 import { NewType } from "@/types";
 
 export const newsFetch = async () => {
-    const response = await fetch(`https://${process.env.VERCEL_URL}/api/news`);
+    const response = await fetch(`https://api/news`);
     const result: NewType[] = await response.json();
     return result;
 }
