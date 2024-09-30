e8921181fdc9102e04aaedd28feac5881a3c4624
baselbalkes <baselbalkes77@gmail.com>
2024-09-30 08:51:57 +0300

    add environment variable for baseUrl

--- src/dataFetching/newsFetch.ts
+++ src/dataFetching/newsFetch.ts
@@ -1,7 +1,8 @@
 import { NewType } from "@/types";
 
 export const newsFetch = async () => {
-    const response = await fetch("https://sport-website-three.vercel.app/api/news");
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+    const response = await fetch(`${baseUrl}/api/news`);
     const result: NewType[] = await response.json();
     return result;
 }
