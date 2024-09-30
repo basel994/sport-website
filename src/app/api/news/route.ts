a7df19e05d743e82f99782c9644f5eb48b1b0f07
baselbalkes <baselbalkes77@gmail.com>
2024-09-30 10:53:58 +0300

    make api working dynamicly

--- src/app/api/news/route.ts
+++ src/app/api/news/route.ts
@@ -1,6 +1,9 @@
 import { sql } from "@vercel/postgres";
+import { NextRequest } from "next/server";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+    const headers = new Headers(request.headers);
+    headers.get("Content-Type");
     const result = await sql `
     SELECT * FROM news
     `;
--- src/dataFetching/newsFetch.ts
+++ src/dataFetching/newsFetch.ts
@@ -2,7 +2,11 @@ import { NewType } from "@/types";
 
 export const newsFetch = async () => {
     const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-    const response = await fetch(`${baseUrl}/api/news`);
+    const response = await fetch(`${baseUrl}/api/news`, {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
     const result: NewType[] = await response.json();
     return result;
 }
