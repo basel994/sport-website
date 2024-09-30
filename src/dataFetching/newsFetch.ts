b282d253c73c026489a25cb8956d176355870f96
baselbalkes <baselbalkes77@gmail.com>
2024-09-30 08:07:35 +0300

    fix api fetch errors

--- src/dataFetching/newsFetch.ts
+++ src/dataFetching/newsFetch.ts
@@ -1,7 +1,7 @@
 import { NewType } from "@/types";
 
 export const newsFetch = async () => {
-    const response = await fetch("/api/news");
+    const response = await fetch("https://sport-website-three.vercel.app/api/news");
     const result: NewType[] = await response.json();
     return result;
 }
