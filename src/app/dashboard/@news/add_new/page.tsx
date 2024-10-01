d00e00c39e795e48c7d39d065eaa7853e20468e5
baselbalkes <baselbalkes77@gmail.com>
2024-10-01 14:10:10 +0300

    remove unused imports

--- src/app/dashboard/@news/add_new/page.tsx
+++ src/app/dashboard/@news/add_new/page.tsx
@@ -1,5 +1,4 @@
 import PanelCard from "@/components/PanelCard/PanelCard";
-import styles from "./../../dashboard.module.css";
 import NewForm from "@/clientComponents/NewForm/NewForm";
 export default function AddNew() {
     return(
