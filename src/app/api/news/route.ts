f49d06ac62fea4028890f87c8e1bbcec6dabd2e0
baselbalkes <baselbalkes77@gmail.com>
2024-10-01 15:26:11 +0300

    add api for add new

--- src/app/api/news/route.ts
+++ src/app/api/news/route.ts
@@ -1,6 +1,12 @@
 import { sql } from "@vercel/postgres";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import fs from "fs";
+import path from "path";
 
+const uploadsDir = path.join(process.cwd(), 'public', 'images', 'home', 'news', 'uploads');  
+if (!fs.existsSync(uploadsDir)) {  
+    fs.mkdirSync(uploadsDir, { recursive: true }); // إنشاء المجلد إذا لم يكن موجودًا  
+}
 export async function GET(request: NextRequest) {
     const headers = new Headers(request.headers);
     headers.get("Content-Type");
@@ -9,4 +15,22 @@ export async function GET(request: NextRequest) {
     `;
     const rows = result.rows;
     return new Response(JSON.stringify(rows));
+}
+export async function POST(request: NextRequest) {
+    const {title, content, image}:{title: string, content: string, image: File} = await request.json();
+    if(title && content && image){
+        const blobImage = image as Blob;
+        const filename = `${Date.now()}-${image.name}`;
+        const filepath = path.join(uploadsDir, filename);
+        const buffer = Buffer.from(await blobImage.arrayBuffer());
+        fs.writeFileSync(filepath, buffer); 
+        const storedPath = `/images/home/news/uploads/${filename}`
+        const add = await sql `
+        INSERT INTO news (title, content, image) VALUES (${title}, ${content}, ${storedPath}) RETURNING id
+        `;
+        return new NextResponse(JSON.stringify({message: "created successfully", id: add.rows[0].id}));
+    }
+    else {
+        return new NextResponse(JSON.stringify({message: "error"}));
+    }
 }
